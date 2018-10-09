cc0a9c7fe0de11ff90c03ab927a49eff1a8fa27e
Денисов Александр Евгеньевич <denisov_ae@taximaxim.local>
2018-10-09 18:44:45 +0300

    add db init
    * add class for generate db create code
    * add script to fill test data
    * change entity field

--- src/main/java/ru/rest/Advert.java
+++ src/main/java/ru/rest/Advert.java
@@ -2,22 +2,49 @@ package ru.rest;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
 
 @Entity
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Advert {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", columnDefinition = "serial")
     private Integer id;
 
     @Column(name = "c_title")
     private String title;
 
-    public Advert(String name, int id) {
-        this.title = name;
+    @Column(name = "c_url")
+    private String url;
+
+    @Column(name = "c_sum")
+    private float sum;
+
+    @Column(name = "c_desc")
+    private String desc;
+
+    @Column(name = "c_date")
+    private  Timestamp dateTime;
+
+    @Column(name = "c_img")
+    private String img;
+
+
+    public Advert() {
+    }
+
+    public Advert(Integer id, String title, String url, float sum, String desc, Timestamp dateTime, String img) {
         this.id = id;
+        this.title = title;
+        this.url = url;
+        this.sum = sum;
+        this.desc = desc;
+        this.dateTime = dateTime;
+        this.img = img;
     }
 
     public String getTitle() {
@@ -26,6 +53,26 @@ public class Advert {
 
     public int getId() {
         return id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public Timestamp getDateTime() {
+        return dateTime;
+    }
+
+    public String getImg() {
+        return img;
     }
 
     @Override
--- src/main/java/ru/rest/AdvertController.java
+++ src/main/java/ru/rest/AdvertController.java
@@ -3,12 +3,14 @@ package ru.rest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
+
 @RestController
 public class AdvertController {
 
     @RequestMapping("/advert")
     public Advert advert() {
-        Advert obj = new Advert("hellow world", 1);
+        Advert obj = new Advert(1, "title", "url", 2005, "description", new Timestamp(1), "1.jpg");
         return obj;
     }
 }
